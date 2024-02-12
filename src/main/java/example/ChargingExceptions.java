0f92b1225bbefd09ce75805db1c82c8f77c77358
MR-KHAN0099 <mohammedrizwan0909@gmail.com>
2024-02-12 18:04:06 +0100

    New File for Task4

---
+++ src/main/java/example/ChargingExceptions.java
@@ -0,0 +1,15 @@
+package example;
+
+public class ChargingExceptions extends Exception {
+ private static final long serialVersionUID = 1L;
+
+ public ChargingExceptions(String message) {
+     super(message);
+ }
+
+ public ChargingExceptions(String message, Throwable cause) {
+     super(message, cause);
+ }
+}
+
+
